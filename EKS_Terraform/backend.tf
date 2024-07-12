d075a95d97d75a750271eecec2578c8e2a4b4d45
Akeem Shokanbi <55434539+akshow37@users.noreply.github.com>
2024-07-11 21:23:49 -0400

    Update backend.tf

--- EKS_Terraform/backend.tf
+++ EKS_Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "yusuf-docker-tfstate" # Replace with your actual S3 bucket name
+    bucket = "olanrewaju-docker-tfstate" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-west-2"
   }
 }
